f7b704213381eb702d174df0b1595b01fa2ece79
Z4 DEV <z4kreyyt@gmail.com>
2023-10-25 17:51:33 +0100

    ValidateAccount Method

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import { validateAccount } from "../repositorys/user.repository";
 
 export const Login = (req: Request, res: Response) => {
-    res.send("test")
+    const user = validateAccount(req.body.email, req.body.password).then((user) => {
+        res.status(200).json({
+            status: 200,
+            message: "Login Success",
+            data: user
+        })
+    }).catch((err) => {
+        res.status(err.status).json({
+            status: err.status,
+            message: err.message
+        })
+    })
 }
--- src/repositorys/user.repository.ts
+++ src/repositorys/user.repository.ts
@@ -1,11 +1,19 @@
 import prisma from "../services/prismaClient";
+import bycrpt from "bcryptjs";
 
-export const checkUser = async (email: string, password: string) => {
-    const user = await prisma.user.findUnique({
-        where: {
-            email,
-        },
-    });
+export const validateAccount = async (email: string, password: string) => {
+    return new Promise(async (resolve, reject) => {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+    
+        if(!user) return reject({ status: 400, message: "Invalid account" });
+    
+        const isValidPassword = await bycrpt.compare(password, user.password);
+    
+        return isValidPassword ? resolve({...user, password: undefined}) : reject({ status: 400, message: "Invalid password" });
+    })
 
-    return user ?? false;
 }
