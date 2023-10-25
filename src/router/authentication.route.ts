82d9b1d801666930209ebccbda97c4cf1319baec
Z4 DEV <z4kreyyt@gmail.com>
2023-10-25 14:00:30 +0100

    Authentication Controller + Route addiction

--- src/router/authentication.route.ts
+++ src/router/authentication.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { limiter } from "../middleware/ratelimit.middleware";
+import * as authenticationController from "../controllers/authentication.controller";
 
 export const router = Router();
 
-router.post('/login', limiter);
+router.post('/login', limiter, authenticationController.Login);
