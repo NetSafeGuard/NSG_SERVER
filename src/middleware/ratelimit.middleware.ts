c955deceed88bfedbb88d71258898733e7f78a01
Z4 DEV <z4kreyyt@gmail.com>
2023-10-26 18:35:03 +0100

    Changing ratelimit time

--- src/middleware/ratelimit.middleware.ts
+++ src/middleware/ratelimit.middleware.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 50,
+    max: 15,
     message: {
         status: 'fail',
         message: 'Too many requests from this IP, please try again in 15 minutes!'
