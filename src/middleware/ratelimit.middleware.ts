08990d6416c38f683a72892c17b75bb9df84d234
João Silva <z4kreyyt@gmail.com>
2023-10-27 01:16:34 +0100

    RateLimit Message

--- src/middleware/ratelimit.middleware.ts
+++ src/middleware/ratelimit.middleware.ts
@@ -1,10 +1,10 @@
 import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
+    windowMs: 5 * 60 * 1000,
     max: 15,
     message: {
         status: 'fail',
-        message: 'Too many requests from this IP, please try again in 15 minutes!'
+        message: 'Muitas requisições, tente novamente em 5 minutos!'
     },
 });
