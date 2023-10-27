cdbf8aae7ad545fb74b5b94a0d991b43e88d0cdb
João Silva <z4kreyyt@gmail.com>
2023-10-27 01:14:44 +0100

    Changing Messages

--- src/repositorys/user.repository.ts
+++ src/repositorys/user.repository.ts
@@ -10,12 +10,12 @@ export const validateAccount = async (email: string, password: string) => {
             },
         });
     
-        if(!user) return reject({ status: 400, message: "Invalid account" });
+        if(!user) return reject({ status: 400, message: "Conta não encontrada!" });
     
         const isValidPassword = await bycrpt.compare(password, user.password);
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET ?? 'SCR_2023', { expiresIn: "1d" });
 
-        return isValidPassword ? resolve({ user:{...user, password: undefined}, token }) : reject({ status: 400, message: "Invalid password" });
+        return isValidPassword ? resolve({ user:{...user, password: undefined}, token }) : reject({ status: 400, message: "Palavra-passe inválida" });
     })
 
 }
