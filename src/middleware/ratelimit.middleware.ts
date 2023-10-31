91d8ef036fec988041c5cd82bd3fdfaa51c0bbe8
João Silva <z4kreyyt@gmail.com>
2023-10-31 23:35:00 +0000

    RateLimit time change

--- src/middleware/ratelimit.middleware.ts
+++ src/middleware/ratelimit.middleware.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 5 * 60 * 1000,
-    max: 15,
+    max: 5,
     message: {
         status: 'fail',
         message: 'Muitas requisições, tente novamente em 5 minutos!'
