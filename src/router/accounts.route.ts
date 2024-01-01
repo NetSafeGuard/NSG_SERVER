b9aae82f2a1a1d7dd6a1cef4a27cc8bd4eab5f6a
Z4 <z4kreyyt@gmail.com>
2024-01-01 00:39:33 +0000

    Adding [POST] /account/create route

--- src/router/accounts.route.ts
+++ src/router/accounts.route.ts
@@ -4,7 +4,9 @@ import * as Controller from "../useCases/Account/Controller/account.controller";
 import { ValidateMiddleware } from "../middleware/validate.middleware";
 import { LoginSchema } from "../schemas/authentication.schema";
 import { TokenMiddleware } from "../middleware/token.middleware";
+import { AccountSchema } from "../schemas/accountdata.schema";
 
 export const accountsrouter = Router();
 
 accountsrouter.get('/', [TokenMiddleware], Controller.getAccounts);
+accountsrouter.post('/create', [limiter, ValidateMiddleware(AccountSchema)], Controller.Create)
