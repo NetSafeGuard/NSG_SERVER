469c733ca945f57e07490626d6b06cdacd292b21
Z4 <z4kreyyt@gmail.com>
2024-01-20 03:15:36 +0000

    Creating Exam Route + Controller + Repository

--- src/router/exams.route.ts
+++ src/router/exams.route.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import * as examsController from "../useCases/Exams/Controller/exam.controller";
 
 export const examsrouter = Router();
+
+examsrouter.post("/", examsController.Create);
