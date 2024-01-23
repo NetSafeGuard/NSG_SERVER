1731c770cd7c656e10ad499e982bc36b3bca229e
Z4 <z4kreyyt@gmail.com>
2024-01-23 23:30:33 +0000

    Removing some imports

--- src/router/infos.route.ts
+++ src/router/infos.route.ts
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { limiter } from "../middleware/ratelimit.middleware";
 import * as Controller from "../useCases/Infos/Controller";
-import { ValidateMiddleware } from "../middleware/validate.middleware";
 import { LoginSchema } from "../schemas/account.schemas";
 import { TokenMiddleware } from "../middleware/token.middleware";
 
