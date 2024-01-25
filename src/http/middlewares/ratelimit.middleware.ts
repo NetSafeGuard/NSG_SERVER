a93b01abd4e502eb90207c47974883832b88bfc8
Z4 <z4kreyyt@gmail.com>
2024-01-25 21:57:18 +0000

    Remaking RateLimit middleware

--- src/http/middlewares/ratelimit.middleware.ts
+++ src/http/middlewares/ratelimit.middleware.ts
@@ -1,10 +1,24 @@
 import { rateLimit } from 'express-rate-limit';
 
-export const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 10,
+export const limiter = (ratelimit: number, time: number) => rateLimit({
+    windowMs: time,
+    max: ratelimit,
     message: {
-        status: 'fail',
-        message: 'Muitas requisições, tente novamente em 5 minutos!'
+        status: 429,
+        message: "Muitas requisições, tente novamente em " + formatNumberToTime(time),
     },
+    headers: true,
 });
+
+function formatNumberToTime(number: number) {
+    number = number / 1000;
+
+    const minutes = Math.floor(number % 60);
+    const hours = Math.floor(number / 60);
+    
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+  
+    return `${formattedHours}:${formattedMinutes}`;
+  }
+  
