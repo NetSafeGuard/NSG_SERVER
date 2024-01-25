19e61ddb40ce6dbf0925ea659e33973114641b2a
Z4 <z4kreyyt@gmail.com>
2024-01-25 14:48:32 +0000

    Verifying Email Service

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import "dotenv/config";
 import { ExpressServer } from "./models/server.model";
+import { transporter } from "./services/nodeMailer.service";
 
 const server = new ExpressServer();
 server.start();
+
+transporter
+  .verify()
+  .then(() => {
+    console.log("[Email Service] Ready for send emails!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export default server.app;
