57b25a871e9a2a9a00147f6ffffc00bd318e037d
Z4 <z4kreyyt@gmail.com>
2024-01-26 00:55:34 +0000

    Adding `/token` route

--- src/http/models/server.model.ts
+++ src/http/models/server.model.ts
@@ -5,6 +5,7 @@ import { inforouter } from "../routers/infos.route";
 import { LogsMiddleware } from "../middlewares/logs.middleware";
 import cors from "cors";
 import { examsrouter } from "../routers/exams.route";
+import { tokenrouter } from "../routers/token.route";
 
 export class ExpressServer {
   public app: express.Application;
@@ -27,10 +28,14 @@ export class ExpressServer {
 
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
+    this.app.set('view engine', 'ejs');
+    this.app.set('views', './src/http/views');
+
     this.app.use("/api/v1/auth", authrouter);
     this.app.use("/api/v1/account", accountsrouter);
     this.app.use("/api/v1/infos", inforouter);
     this.app.use("/api/v1/exams", examsrouter);
+    this.app.use("/token/", tokenrouter)
 
     this.app.all("/*", this.notfound);
 
