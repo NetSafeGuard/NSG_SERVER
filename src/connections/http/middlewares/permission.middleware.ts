db1f31f82dfc99cf9bb32b7e28d12ba7a1ca9d69
=Z4 <z4kreyyt@gmail.com>
2024-03-26 13:36:40 +0000

    feat(permission.middleware): Add Array option to permissions

--- src/connections/http/middlewares/permission.middleware.ts
+++ src/connections/http/middlewares/permission.middleware.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import { Role } from "@prisma/client";
 
-export const PermissionMiddleware = (permission: Role) => {
+type RoleProps = Role | Role[];
+
+export const PermissionMiddleware = (permission: RoleProps) => {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!req.body.user)
       return res.status(400).json({
@@ -9,11 +11,20 @@ export const PermissionMiddleware = (permission: Role) => {
           "[Permissão Negada] Parece que a sua conta não tem permissão para isso :(",
       });
 
-    if (req.body.user.role !== permission) {
-      return res.status(405).json({
-        message:
-          "[Permissão Negada] Parece que a sua conta não tem permissão para isso :(",
-      });
+    if(typeof permission == "string") {
+      if (req.body.user.role !== permission) {
+        return res.status(405).json({
+          message:
+              "[Permissão Negada] Parece que a sua conta não tem permissão para isso :(",
+        });
+      }
+    } else {
+        if (!permission.includes(req.body.user.role)) {
+            return res.status(405).json({
+            message:
+                "[Permissão Negada] Parece que a sua conta não tem permissão para isso :(",
+            });
+        }
     }
 
     next();
