1c41ca97ef83c6448b7d7b09b8deb41d065356eb
Z4dEV <z4kreyyt@gmail.com>
2024-04-09 14:50:41 +0100

    Refactor group route to use Delete instead of Del in groups.route.ts

--- src/connections/http/routers/groups.route.ts
+++ src/connections/http/routers/groups.route.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { limiter } from "../middlewares/ratelimit.middleware";
-import { Create, Get, Del } from "../useCases/Groups/Controller/group.controller";
+import { Create, Get, Delete } from "../useCases/Groups/Controller/group.controller";
+import * as StudentController from "../useCases/Students/Controller/student.controller";
 import { ValidateMiddleware } from "../middlewares/validate.middleware";
 import { TokenMiddleware } from "../middlewares/token.middleware";
-import { CreateGroupSchema } from "../schemas/group.schema";
+import { CreateGroupSchema, CreateStudentSchema } from "../schemas/group.schema";
 import { PermissionMiddleware } from "../middlewares/permission.middleware";
 
 export const groupsrouter = Router();
 
 groupsrouter.post("/", [TokenMiddleware, limiter(10,60000), PermissionMiddleware("ADMIN"), ValidateMiddleware(CreateGroupSchema)], Create);
 groupsrouter.get("/", [TokenMiddleware, PermissionMiddleware(["USER", "ADMIN"])], Get);
-groupsrouter.delete('/',[TokenMiddleware, limiter(10,60000), PermissionMiddleware("ADMIN"), ValidateMiddleware(CreateGroupSchema)], Del);
+groupsrouter.delete("/",[TokenMiddleware, limiter(10,60000), PermissionMiddleware("ADMIN"), ValidateMiddleware(CreateGroupSchema)], Delete);
+// groupsrouter.put("/",[TokenMiddleware, limiter(10,60000), PermissionMiddleware("ADMIN"), ValidateMiddleware(EditGroupSchema)], Delete)
+groupsrouter.post('/student', [TokenMiddleware, limiter(10,60000), PermissionMiddleware("ADMIN"), ValidateMiddleware(CreateStudentSchema)], StudentController.Create);
