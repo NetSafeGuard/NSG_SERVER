3bc712e908a7fcfa771af7ff6a45b9a880746f07
Z4dEV <z4kreyyt@gmail.com>
2024-04-09 14:51:00 +0100

    Add validation schema for creating a student in group.schema.ts

--- src/connections/http/schemas/group.schema.ts
+++ src/connections/http/schemas/group.schema.ts
@@ -3,3 +3,11 @@ import * as yup from 'yup';
 export const CreateGroupSchema = yup.object({
     name: yup.string().required(),
 });
+
+export const CreateStudentSchema = yup.object({
+    groupname: yup.string().required(),
+    name: yup.string().required(),
+    studentid: yup.string().required(),
+    email: yup.string().required(),
+    routerip: yup.string().required(),
+});
