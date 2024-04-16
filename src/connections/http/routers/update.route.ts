c9f5e6d708f6229af7334caa09abed2de892c8e1
Z4dEV <z4kreyyt@gmail.com>
2024-04-16 15:10:21 +0100

    Add download and signature verification logic to update route

--- src/connections/http/routers/update.route.ts
+++ src/connections/http/routers/update.route.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import download from 'download';
 
 export const updaterouter = Router();
 
@@ -10,22 +11,27 @@ interface Props {
 
 updaterouter.get('/', (req: Request<{}, {}, {}, Props>, res: Response) => {
     if(req.query.current_version && req.query.target && req.query.arch) {
-        GithubLatest().then((release: any) => {
+        GithubLatest().then(async (release: any) => {
             if(release.name !== req.query.current_version) {
                 if(req.query.target == "windows") {
-                    res.status(200).send({
-                        url: release.assets[0].browser_download_url,
-                        version: release.name,
-                        pub_date: release.published_at,
-                        signature: release.assets[1].browser_download_url
+                    let url ="";
+                    release.assets.forEach((asset: any) => {
+                        if(asset.name.endsWith(".sig")) {
+                            url = asset.url;
+                        }
                     })
 
-                    console.log({
+                    let data: any = await getSignature(url);
+                    console.log(data)
+
+                   res.status(200).send({
                         url: release.assets[0].browser_download_url,
                         version: release.name,
                         pub_date: release.published_at,
-                        signature: release.assets[1].browser_download_url
+                        signature: (await download(data.browser_download_url)).toString() || null
                     })
+
+                    console.log((await download(data.browser_download_url)).toString() || null)
 
                 }
                 else {
@@ -45,5 +51,10 @@ updaterouter.get('/', (req: Request<{}, {}, {}, Props>, res: Response) => {
 
 const GithubLatest = async () => {
     const response = await fetch("https://api.github.com/repos/NetSafeGuard/NSG_ADMIN/releases/latest");
+    return response.json();
+}
+
+const getSignature = async (url: string) => {
+    const response = await fetch(url);
     return response.json();
 }
