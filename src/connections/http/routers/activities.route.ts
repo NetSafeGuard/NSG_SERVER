5928033604dbf7b1913ad6faf2233e4a7d3463ea
Z4dEV <z4kreyyt@gmail.com>
2024-05-22 15:48:37 +0100

    chore: Add rate limiting, validation, and permission checks to create activity route

--- src/connections/http/routers/activities.route.ts
+++ src/connections/http/routers/activities.route.ts
@@ -1,6 +1,18 @@
 import { Router } from "express";
 import * as activityController from "../useCases/Activities/Controller/activity.controller";
+import { ValidateMiddleware } from "../middlewares/validate.middleware";
+import { CreateActivitySchema } from "../schemas/activity.schema";
+import { limiter } from "../middlewares/ratelimit.middleware";
+import { PermissionMiddleware } from "../middlewares/permission.middleware";
 
 export const activityrouter = Router();
 
-activityrouter.post("/", activityController.Create);
+activityrouter.post(
+    "/",
+    [
+        limiter(6, 60000),
+        ValidateMiddleware(CreateActivitySchema),
+        PermissionMiddleware(["ADMIN", "USER"])
+    ], 
+    activityController.Create
+);
---
+++ src/connections/http/schemas/activity.schema.ts
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+export const CreateActivitySchema = yup.object().shape({
+    title: yup.string().required(),
+    description: yup.string().required(),
+    startdate: yup.date().required(),
+    enddate: yup.date().required(),
+    groups: yup.array().of(yup.string()).required(),
+});
