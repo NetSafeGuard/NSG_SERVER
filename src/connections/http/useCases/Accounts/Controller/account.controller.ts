d0b79d843befe7bd2a07ffe33e473a8a3ec932dd
Z4dEV <z4kreyyt@gmail.com>
2024-06-12 16:50:01 +0100

    feat: new email pass & uuid on url

--- src/connections/http/useCases/Accounts/Controller/account.controller.ts
+++ src/connections/http/useCases/Accounts/Controller/account.controller.ts
@@ -1,235 +1,204 @@
-import type { Request, Response } from "express";
+import type { Request, Response } from 'express';
 import {
-  createUser,
-  getAccount,
-  getUserByEmailOrUsername,
-  getUsers,
-  updateUser,
-  deleteUser,
-  activeUser
-} from "../Repository/account.repository";
-import bycrpt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { RevoveryTemplate } from "@http/templates/recovery.template";
-import { createToken, getAllTokens } from "../../Tokens/Repository/token.repository";
-import { wss } from "@/index";
+	createUser,
+	getAccount,
+	getUserByEmailOrUsername,
+	getUsers,
+	updateUser,
+	deleteUser,
+	activeUser,
+} from '../Repository/account.repository';
+import bycrpt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { RevoveryTemplate } from '@http/templates/recovery.template';
+import { v4 } from 'uuid';
+import { createToken } from '../../Tokens/Repository/token.repository';
+import { wss } from '@/index';
 
 export const Login = async (req: Request, res: Response) => {
-  const account = await getAccount(req.body.username);
+	const account = await getAccount(req.body.username);
 
-  if (!account)
-    return res.status(400).json({
-      status: 400,
-      message: "Conta não encontrada",
-    });
+	if (!account)
+		return res.status(400).json({
+			status: 400,
+			message: 'Conta não encontrada',
+		});
 
-  const isValidPassword = await bycrpt.compare(
-    req.body.password,
-    account.password
-  );
+	const isValidPassword = await bycrpt.compare(req.body.password, account.password);
 
-  if (!isValidPassword)
-    return res.status(400).json({
-      status: 400,
-      message: "Palavra-passe inválida",
-    });
+	if (!isValidPassword)
+		return res.status(400).json({
+			status: 400,
+			message: 'Palavra-passe inválida',
+		});
 
-  const token = jwt.sign(
-    { email: account.email },
-    process.env.JWT_SECRET ?? "SCR_2023",
-    { expiresIn: "1d" }
-  );
+	const token = jwt.sign({ email: account.email }, process.env.JWT_SECRET ?? 'SCR_2023', {
+		expiresIn: '1d',
+	});
 
-  res.status(200).json({
-    status: 200,
-    message: "Login Success",
-    data: {
-      user: {
-        email: account.email,
-      },
-      token,
-    },
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Login Success',
+		data: {
+			user: {
+				email: account.email,
+			},
+			token,
+		},
+	});
 };
 
 export const Verify = (req: Request, res: Response) => {
-  res.status(200).json({
-    status: 200,
-    message: "Verify Success",
-    data: {
-      user: {
-        ...req.body.user,
-      },
-    },
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Verify Success',
+		data: {
+			user: {
+				...req.body.user,
+			},
+		},
+	});
 };
 
 export const Create = async (req: Request, res: Response) => {
-  const account = await getUserByEmailOrUsername(
-    req.body.email,
-    req.body.username
-  );
+	const account = await getUserByEmailOrUsername(req.body.email, req.body.username);
 
-  if (account)
-    return res.status(400).json({
-      status: 400,
-      message: "Conta já existente!",
-    });
+	if (account)
+		return res.status(400).json({
+			status: 400,
+			message: 'Conta já existente!',
+		});
 
-  const newUser = await createUser(
-    req.body.email,
-    req.body.username,
-    req.body.password ?? "admin"
-  );
+	const newUser = await createUser(
+		req.body.email,
+		req.body.username,
+		req.body.password ?? 'admin',
+	);
 
-  const token = jwt.sign(
-    { email: newUser },
-    process.env.JWT_SECRET ?? "SCR_2023",
-    { expiresIn: "1d" }
-  );
+	const token = jwt.sign({ email: newUser }, process.env.JWT_SECRET ?? 'SCR_2023', {
+		expiresIn: '1d',
+	});
 
-  res.status(200).json({
-    status: 200,
-    message: "Create Success",
-    data: {
-      user: {
-        email: newUser,
-      },
-      token,
-    },
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Create Success',
+		data: {
+			user: {
+				email: newUser,
+			},
+			token,
+		},
+	});
 
-  getUsers().then((users) => {
-    wss.io.emit("users", users);
-  });
+	getUsers().then(users => {
+		wss.io.emit('users', users);
+	});
 };
 
 export const Update = async (req: Request, res: Response) => {
-  const account = await getUserByEmailOrUsername(req.body.old_email, "");
+	const account = await getUserByEmailOrUsername(req.body.old_email, '');
 
-  if (!account)
-    return res.status(400).json({
-      status: 400,
-      message: "Conta não existente!",
-    });
+	if (!account)
+		return res.status(400).json({
+			status: 400,
+			message: 'Conta não existente!',
+		});
 
-  const newaccount = await getUserByEmailOrUsername(req.body.email, "");
+	const newaccount = await getUserByEmailOrUsername(req.body.email, '');
 
-  if (newaccount && newaccount.email !== req.body.old_email)
-    return res.status(400).json({
-      status: 400,
-      message: "Já existe uma conta com esse email ou nome de utilizador!",
-    });
+	if (newaccount && newaccount.email !== req.body.old_email)
+		return res.status(400).json({
+			status: 400,
+			message: 'Já existe uma conta com esse email ou nome de utilizador!',
+		});
 
-  const newUser = await updateUser(
-    req.body.old_email,
-    req.body.email,
-    req.body.username,
-    req.body.avatar,
-    req.body.role
-  );
+	const newUser = await updateUser(
+		req.body.old_email,
+		req.body.email,
+		req.body.username,
+		req.body.avatar,
+		req.body.role,
+	);
 
-  res.status(200).json({
-    status: 200,
-    message: "Update Success",
-    data: {
-      user: {
-        email: newUser,
-      },
-    },
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Update Success',
+		data: {
+			user: {
+				email: newUser,
+			},
+		},
+	});
 
-  getUsers().then((users) => {
-    wss.io.emit("users", users);
-  });
+	getUsers().then(users => {
+		wss.io.emit('users', users);
+	});
 };
 
 export const Delete = async (req: Request, res: Response) => {
-  const account = await getUserByEmailOrUsername(
-    req.body.email,
-    req.body.username
-  );
+	const account = await getUserByEmailOrUsername(req.body.email, req.body.username);
 
-  if (!account)
-    return res.status(400).json({
-      status: 400,
-      message: "Conta não existente!",
-    });
+	if (!account)
+		return res.status(400).json({
+			status: 400,
+			message: 'Conta não existente!',
+		});
 
-  const newUser = await deleteUser(req.body.email);
+	const newUser = await deleteUser(req.body.email);
 
-  res.status(200).json({
-    status: 200,
-    message: "Delete Success",
-    data: {
-      user: {
-        email: newUser,
-      },
-    },
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Delete Success',
+		data: {
+			user: {
+				email: newUser,
+			},
+		},
+	});
 
-  getUsers().then((users) => {
-    wss.io.emit("users", users);
-  });
+	getUsers().then(users => {
+		wss.io.emit('users', users);
+	});
 };
 
 export const Active = async (req: Request, res: Response) => {
+	if (req.body.activated)
+		return res.status(400).json({
+			status: 400,
+			message: 'Conta já ativada!',
+		});
 
-  if (req.body.activated)
-    return res.status(400).json({
-      status: 400,
-      message: "Conta já ativada!",
-    });
+	const newUser = await activeUser(req.body.user.email, req.body.password);
 
-  const newUser = await activeUser(req.body.user.email, req.body.password);
-
-  res.status(200).json({
-    status: 200,
-    message: "Active Success",
-    data: {
-      user: {
-        email: newUser,
-      },
-    },
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Active Success',
+		data: {
+			user: {
+				email: newUser,
+			},
+		},
+	});
 };
 
 export const Recover = async (req: Request, res: Response) => {
-  const getAccount = await getUserByEmailOrUsername(req.body.email, "");
+	const getAccount = await getUserByEmailOrUsername(req.body.email, '');
 
-  if (!getAccount) return res.status(400).json({
-    status: 400,
-    message: "Conta não existente!",
-  });
+	if (!getAccount)
+		return res.status(400).json({
+			status: 400,
+			message: 'Conta não existente!',
+		});
 
-  const tokens = await getAllTokens();
+	const token = v4();
 
-  let token = generateStringCode();
+	await createToken(token, req.body.email);
 
-  while (tokens.find((t) => t.token === token)) {
-    token = generateStringCode();
-  }
+	const email = new RevoveryTemplate(req.body.email, token);
+	email.sendEmail(req.body.email);
 
-  await createToken(token, req.body.email);
-
-  const email = new RevoveryTemplate(req.body.email, token);
-  email.sendEmail(req.body.email);
-
-  res.status(200).json({
-    status: 200,
-    message: "Email enviado com sucesso!",
-  });
+	res.status(200).json({
+		status: 200,
+		message: 'Email enviado com sucesso!',
+	});
 };
-
-function generateStringCode() {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  let code = "";
-
-  for (let i = 0; i < 5; i++) {
-    code += letters.charAt(Math.floor(Math.random() * letters.length));
-  }
-
-
-  return code;
-}
--- src/connections/http/useCases/Accounts/Repository/account.repository.ts
+++ src/connections/http/useCases/Accounts/Repository/account.repository.ts
@@ -1,120 +1,138 @@
-import { v4 } from "uuid";
-import prisma from "../../../services/prismaClient.service";
-import bycrpt from "bcryptjs";
-import type { Role } from "@prisma/client";
+import { v4 } from 'uuid';
+import prisma from '../../../services/prismaClient.service';
+import bycrpt from 'bcryptjs';
+import type { Role } from '@prisma/client';
+import { getAllTokens } from '../../Tokens/Repository/token.repository';
 
 export const getAccount = async (account: string) => {
-    const user = await prisma.user.findFirst({
-        where: {
-            OR: [
-                { username: account },
-                { email: account }
-            ]
-        },
-    });
+	const user = await prisma.user.findFirst({
+		where: {
+			OR: [{ username: account }, { email: account }],
+		},
+	});
 
-    return user
-}
+	return user;
+};
 
 export const getUserByEmailOrUsername = async (email: string, username: string) => {
-    const user = await prisma.user.findFirst({
-        where: {
-            OR: [
-                { username },
-                { email }
-            ]
-        },
-    });
+	const user = await prisma.user.findFirst({
+		where: {
+			OR: [{ username }, { email }],
+		},
+	});
 
-    return user
-}
+	return user;
+};
 
 export const createUser = async (email: string, username: string, password: string) => {
-    const hashedPassword = await bycrpt.hash(password, 10);
+	const hashedPassword = await bycrpt.hash(password, 10);
 
-    const newUser = await prisma.user.create({
-        data: {
-            email,
-            username,
-            password: hashedPassword,
-            role: "USER"
-        }
-    });
+	const newUser = await prisma.user.create({
+		data: {
+			email,
+			username,
+			password: hashedPassword,
+			role: 'USER',
+		},
+	});
 
-    return newUser.email;
-}
+	return newUser.email;
+};
 
-export const updateUser = async (old_email: string, email: string, username: string, avatar: string, role: string) => {
-    const newUser = await prisma.user.update({
-        where: {
-            email: old_email
-        },
-        data: {
-            username,
-            avatar,
-            email,
-            role: role as Role
-        }
-    });
+export const updateUser = async (
+	old_email: string,
+	email: string,
+	username: string,
+	avatar: string,
+	role: string,
+) => {
+	const newUser = await prisma.user.update({
+		where: {
+			email: old_email,
+		},
+		data: {
+			username,
+			avatar,
+			email,
+			role: role as Role,
+		},
+	});
 
-    return newUser.email;
-}
+	return newUser.email;
+};
 
 export const deleteUser = async (email: string) => {
-    const user = await prisma.user.delete({
-        where: {
-            email
-        }
-    });
+	const user = await prisma.user.delete({
+		where: {
+			email,
+		},
+	});
 
-    return user.email;
-}
+	return user.email;
+};
 
 export const getUsers = async () => {
-    const users = await prisma.user.findMany({
-        select: {
-            email: true,
-            username: true,
-            role: true,
-            avatar: true,
-            createdAt: true,
-        }
-    });
+	const users = await prisma.user.findMany({
+		select: {
+			email: true,
+			username: true,
+			role: true,
+			avatar: true,
+			createdAt: true,
+		},
+	});
 
-    return users
-}
+	return users;
+};
 
 export const activeUser = async (email: string, password: string) => {
-    const hashedPassword = await bycrpt.hash(password, 10);
+	const hashedPassword = await bycrpt.hash(password, 10);
 
-    const newUser = await prisma.user.update({
-        where: {
-            email
-        },
-        data: {
-            password: hashedPassword,
-            activated: true
-        }
-    });
+	const newUser = await prisma.user.update({
+		where: {
+			email,
+		},
+		data: {
+			password: hashedPassword,
+			activated: true,
+		},
+	});
 
-    return newUser;
-}
+	return newUser;
+};
 
 export const recoverUser = async (email: string) => {
+    const tokens = await getAllTokens();
 
-    const newpass = v4();
+	let token = generateStringCode();
 
-    const password = bycrpt.hashSync(newpass, 10);
+	while (tokens.find(t => t.token === token)) {
+		token = generateStringCode();
+	}
 
-    const newUser = await prisma.user.update({
-        where: {
-            email
-        },
-        data: {
-            password,
-            activated: false
-        }
-    });
+	const password = bycrpt.hashSync(token, 10);
 
-    return newpass;
+	const newUser = await prisma.user.update({
+		where: {
+			email,
+		},
+		data: {
+			password,
+			activated: false,
+		},
+	});
+
+	return token;
+};
+
+function generateStringCode() {
+	const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	let code = '';
+
+	for (let i = 0; i < 5; i++) {
+		code += letters.charAt(Math.floor(Math.random() * letters.length));
+	}
+
+	return code;
 }
